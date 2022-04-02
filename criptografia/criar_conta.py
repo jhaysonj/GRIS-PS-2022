3767d0a2d918838292610f60f4ca07477da4907d
jhaysonj <jhaysonbj@dcc.ufrj.br>
2022-04-02 14:23:57 -0300

    arquivo que quebra a cifra de cesar do registro de conta

--- criptografia/criar_conta.py
+++ criptografia/criar_conta.py
@@ -1,36 +1,36 @@
 # esse arquivo foi necessario para criar a conta no site, nao possui relacao direta com a tag.
 
-def encrypt(chave, mensagem):
+def encriptar(chave, mensagem):
     mensagem = mensagem.upper()
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    result = ""
+    resultado = ""
 
     for letra in mensagem:
         if letra in alpha: # verifica se a letra eh realmente uma letra
             # acha a letra da cifra correspondendo ao nosso alfabeto
-            letra_index = (alpha.find(letra) + chave) % len(alpha)
+            index_letra = (alpha.find(letra) + chave) % len(alpha)
 
-            result = result + alpha[letra_index]
+            resultado = resultado + alpha[index_letra]
         else:
-            result = result + letra
+            resultado = resultado + letra
 
-    return result
+    return resultado
 
-def decrypt(chave, mensagem):
+def decriptar(chave, mensagem):
     mensagem = mensagem.upper()
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    result = ""
+    resultado = ""
 
     for letra in mensagem:
         if letra in alpha: # verifica se a letra eh realmente uma letra
             # acha a letra da cifra correspondendo ao nosso alfabeto 
-            letra_index = (alpha.find(letra) - chave) % len(alpha)
+            index_letra = (alpha.find(letra) - chave) % len(alpha)
 
-            result = result + alpha[letra_index]
+            resultado = resultado + alpha[index_letra]
         else:
-            result = result + letra
+            resultado = resultado + letra
 
-    return result
+    return resultado
 
 frase = "VAUZ XMPPQD MYMLUZS OTQR"
 
@@ -39,9 +39,9 @@ frase = "VAUZ XMPPQD MYMLUZS OTQR"
 No alfabeto temos um ciclo, onde 1 = 27, a chave 2 = 28, a chave 3 = 29. Por isso basta procurarmos ate a chave 26
 """
 for c in range(1,27):
-    print(print(decrypt(c,frase)))
+    print(print(decriptar(c,frase)))
 
 
 print("\n\n")
 print(f"chave correta --> {12}")
-print(f"mensagem da chave correta --> {decrypt(12,frase)}")
+print(f"mensagem da chave correta --> {decriptar(12,frase)}")
